a58377681818b94dcf7c61cb20609dde00ca044a
Andre Fecteau <andre@localhost.localdomain>
2017-12-27 16:01:24 -0500

    minor

--- Solver/Marching.h
+++ Solver/Marching.h
@@ -174,7 +174,7 @@ void Marching<global_solution_vector_type>::timemarch(double time_frame, global_
       global_flux_vector[i] += parabolic_flux.flux(global_solution_vector[i-1], global_solution_vector[i], global_solution_vector[i+1], gamma, Le, Pr, dx) * dt;
       global_flux_vector[i][4] = 0.0;
       global_flux_vector[i] += sources.flux(global_solution_vector[i], gamma, Q, Lambda, theta) * dt;
-      global_flux_vector[i][4] /= dt;
+      // global_flux_vector[i][4] /= dt;
     }
 
 #pragma omp for reduction (+:residual)
